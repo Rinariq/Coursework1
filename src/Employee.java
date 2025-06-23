301b5adcd64915003d1db520c6499659de48ab23
rinariq <RegiYagu17@gmail.com>
2025-06-23 22:59:23 +0300

    Задания Повышенной сложности сделаны

--- src/Employee.java
+++ src/Employee.java
@@ -10,7 +10,7 @@ public class Employee {
     private int id;
     private static int idEmployee = 1;
 
-    public Employee(String nameEmployee, String patronymicEmployee, String surnameEmployee, int department, int wage){
+    public Employee(String nameEmployee, String patronymicEmployee, String surnameEmployee, int department, int wage) {
         this.nameEmployee = nameEmployee;
         this.patronymicEmployee = patronymicEmployee;
         this.surnameEmployee = surnameEmployee;
@@ -19,35 +19,35 @@ public class Employee {
         this.id = idEmployee++;
     }
 
-    public String getNameEmployee(){
+    public String getNameEmployee() {
         return nameEmployee;
     }
 
-    public  String getPatronymicEmployee(){
+    public String getPatronymicEmployee() {
         return patronymicEmployee;
     }
 
-    public String getSurnameEmployee(){
+    public String getSurnameEmployee() {
         return surnameEmployee;
     }
 
-    public int getDepartment(){
+    public int getDepartment() {
         return department;
     }
 
-    public int getWage(){
+    public int getWage() {
         return wage;
     }
 
-    public int getIdEmployee(){
+    public int getIdEmployee() {
         return idEmployee;
     }
 
-    public void setDepartment(int department){
+    public void setDepartment(int department) {
         this.department = department;
     }
 
-    public void setWage (int wage){
+    public void setWage(int wage) {
         this.wage = wage;
     }
 
@@ -70,7 +70,15 @@ public class Employee {
 
     @Override
     public String toString() {
-        return "ID: "+id+"  ФИО СОТРУДНИКА: "+nameEmployee+" "+patronymicEmployee+" "+surnameEmployee+"  № ОДТЕЛА: "+department+" ЗАРПЛАТА: "+ wage+" руб.";
+        return "ID: " + id + "  ФИО СОТРУДНИКА: " + nameEmployee + " " + patronymicEmployee + " " + surnameEmployee + "  № ОДТЕЛА: " + department + "  ЗАРПЛАТА: " + wage + " руб.";
     }
 
+    public void increaseWage(int increment) {
+        this.wage += wage / 100 * increment;
+    }
+
+
+    public String toShotString() {
+        return "ID: " + id + "  ФИО СОТРУДНИКА: " + nameEmployee + " " + patronymicEmployee + " " + surnameEmployee + "  ЗАРПЛАТА: " + wage + " руб.";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,7 @@ public class Main {
         EMPLOYEES[8] = new Employee("Лебедев", "Иван", "Алексеевич", 5, 103200);
         EMPLOYEES[9] = new Employee("Козлова", "Татьяна", "Сергеевна", 3, 59500);
 
+        // базовая сложность
         print();
         System.out.println("\nСумма зарплат: " + findSumWages() + " руб.");
         System.out.println("\nСотрудник с минимальной ЗП:" + "\n" + findEmployeeWithMinWage());
@@ -22,6 +23,26 @@ public class Main {
         System.out.println("\nСредняя зарплата сотрудников: " + findAverageWage() + " руб.");
         System.out.println("\nСписок сотрудников:");
         makeListNPS();
+        System.out.println("");
+
+        //повышенная сложность
+        indexWage();
+        int numberDepartment = 1;
+        int index = 10;
+        System.out.println("\nСотрудник с минимальной ЗП в " + numberDepartment + " отделе: \n" + findEmployeeWithMinWageInDepartament(numberDepartment));
+        System.out.println("\nСотрудник с максимальной ЗП в " + numberDepartment + " отделе: \n" + findEmployeeWithMaxWageInDepartament(numberDepartment));
+        System.out.println("\nСумма ЗП сотрудников в " + numberDepartment + " отделе: \n" + findSumWagesInDepartment(numberDepartment) + " руб.");
+        System.out.println("\nСредняя ЗП сотрудников в " + numberDepartment + " отделе: \n" + findAverageWageInDepartment(numberDepartment) + " руб.");
+        System.out.println("\nСотрудники и их ЗП после индексации на " + index + "% в " + numberDepartment + " отделе:");
+        indexWageInDepartment(index, numberDepartment);
+        System.out.println("\nСотрудники " + numberDepartment + " отдела: ");
+        employeesInDepartment(numberDepartment);
+        int wageBelowAndMore = 100000;
+        System.out.println("\nСотрудники с ЗП меньше " + wageBelowAndMore + " руб:");
+        findEmployeeWithWageBelow(wageBelowAndMore);
+        System.out.println("\nСотрудники с ЗП больше " + wageBelowAndMore + " руб:");
+        findEmployeeWithWageMore(wageBelowAndMore);
+
     }
 
     private static void print() {
@@ -67,6 +88,88 @@ public class Main {
     private static void makeListNPS() {
         for (Employee employee : EMPLOYEES) {
             System.out.println(employee.getNameEmployee() + " " + employee.getPatronymicEmployee() + " " + employee.getSurnameEmployee());
+        }
+    }
+
+    private static void indexWage() {
+        for (Employee employee : EMPLOYEES) {
+            employee.increaseWage(13);
+            System.out.println(employee);
+        }
+    }
+
+    private static Employee findEmployeeWithMinWageInDepartament(int numDepartment) {
+        Employee employeeWithMinWageInDepartment = null;
+        for (Employee employee : EMPLOYEES) {
+            if ((employeeWithMinWageInDepartment == null || employee.getWage() < employeeWithMinWageInDepartment.getWage()) && employee.getDepartment() == numDepartment) {
+                employeeWithMinWageInDepartment = employee;
+            }
+        }
+        return employeeWithMinWageInDepartment;
+    }
+
+    private static Employee findEmployeeWithMaxWageInDepartament(int numDepartment) {
+        Employee employeeWithMaxWageInDepartment = null;
+        for (Employee employee : EMPLOYEES) {
+            if ((employeeWithMaxWageInDepartment == null || employee.getWage() > employeeWithMaxWageInDepartment.getWage()) && employee.getDepartment() == numDepartment) {
+                employeeWithMaxWageInDepartment = employee;
+            }
+        }
+        return employeeWithMaxWageInDepartment;
+    }
+
+    private static int findSumWagesInDepartment(int numDepartment) {
+        int sum = 0;
+        for (Employee employee : EMPLOYEES) {
+            if (employee.getDepartment() == numDepartment) {
+                sum += employee.getWage();
+            }
+        }
+        return sum;
+    }
+
+    private static int findAverageWageInDepartment(int numDepartment) {
+        int sumWageInDepartment = findSumWagesInDepartment(numDepartment);
+        int staffEmployeeInDepartment = 0;
+        for (Employee employee : EMPLOYEES) {
+            if (employee.getDepartment() == numDepartment) {
+                staffEmployeeInDepartment += 1;
+            }
+        }
+        int averageWageInDepartment = sumWageInDepartment / staffEmployeeInDepartment;
+        return averageWageInDepartment;
+    }
+
+    private static void indexWageInDepartment(int indexWage, int numDepartment) {
+        for (Employee employee : EMPLOYEES) {
+            if (employee.getDepartment() == numDepartment) {
+                employee.increaseWage(indexWage);
+                System.out.println(employee);
+            }
+        }
+    }
+
+    private static void employeesInDepartment(int numDepartment) {
+        for (Employee employee : EMPLOYEES) {
+            if (employee.getDepartment() == numDepartment) {
+                System.out.println(employee.toShotString());
+            }
+        }
+    }
+
+    private static void findEmployeeWithWageBelow(int wageBelow) {
+        for (Employee employee : EMPLOYEES) {
+            if (employee.getWage() < wageBelow) {
+                System.out.println(employee);
+            }
+        }
+    }
+
+    private static void findEmployeeWithWageMore(int wageMore) {
+        for (Employee employee : EMPLOYEES) {
+            if (employee.getWage() > wageMore) {
+                System.out.println(employee);
+            }
         }
     }
 }
